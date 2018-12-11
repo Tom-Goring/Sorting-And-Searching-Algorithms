a3f0b44a9426288780006afcfd3c2e7262356119
Tom Goring <tgoring97@gmail.com>
2018-12-11 20:02:26 +0000

    Add header to QuickSort.py

---
+++ QuickSort.py
@@ -0,0 +1,7 @@
+"""Quick Sort breaks down a given list into smaller sub-lists, and then these smaller lists are sorted either
+recursively or iteratively until they are all sorted. All the lists are then appended into one fully sorted list.
+"""
+import random
+
+def quick_sort(alist):
+
