a51d603a9cd09b6ac8fbb8e4e4336a0a942ac168
Tom Goring <tgoring97@gmail.com>
2018-12-12 19:40:22 +0000

    Add comments to aid understanding of merging process in MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -17,10 +17,16 @@ def merge_sort(alist):
         merge_sort(right)
 
         i = j = k = 0
+        # k keeps track of what slot in alist we are putting an element into
+        # i keeps track of what slot in left we are currently comparing
+        # j keeps track of what slot in right we are currently comparing
 
-        # merge left and right into one sorted list
+        # iterate through left and right one item at a time, constantly putting the smallest value from between the two
+        # into the leftmost slot of alist
         while i < len(left) and j < len(right):
 
+            # if the latest item being considered from left is smaller than the latest item on the right, we add it to
+            # the current slot being vied for in alist, else we add the one from right.
             if left[i] < right[j]:
 
                 alist[k] = left[i]
@@ -33,12 +39,14 @@ def merge_sort(alist):
 
             k += 1
 
+        # now we add all remaining items from left into alist
         while i < len(left):
 
             alist[k] = left[i]
             i += 1
             k += 1
 
+        # and the same from right
         while j < len(right):
 
             alist[k] = right[j]
