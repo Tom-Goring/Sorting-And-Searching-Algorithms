a7e98b6cfa169b846639e57d3a96363c0a4954fd
Tom Goring <tgoring97@gmail.com>
2018-12-12 19:01:17 +0000

    Fix format mistake in header of QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -1,4 +1,5 @@
-"""Quick Sort breaks down a given list into smaller sub-lists, and then these smaller lists are sorted either
+"""
+Quick Sort breaks down a given list into smaller sub-lists, and then these smaller lists are sorted either
 recursively or iteratively until they are all sorted. All the lists are then appended into one fully sorted list.
 """
 import random
